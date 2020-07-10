0c235bd0b67521d39d00f2a1251d68e7757514ea
Marlin Kreatif <marlina.kreatif@gmail.com>
2020-07-10 11:35:59 +0700

    update halaman login

--- src/login/Login.js
+++ src/login/Login.js
@@ -1,8 +1,28 @@
 import React, { Component } from 'react';
 import "./login.css";
+import { Form, Col, Button } from "react-bootstrap";
 
 export default class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            validated: false
+        }
+    }
+
+    onSubmit = (event) => {
+
+        let formData = event.target;
+        if (!formData.checkValidity() === false) {
+            event.stopPropagation();
+        }
+        
+        this.setState({ validated: true })
+        event.preventDefault();
+    }
+
     render() {
+        const { validated } = this.state
         return (
             <div className="login-body">
                 <div className="bg-overlay-trs"></div>
@@ -15,6 +35,36 @@ export default class Login extends Component {
                         <span id="sekolah">{"SD Negeri 01 Cibenda".toLocaleUpperCase()}</span>
                     </div>
                     <hr />
+                    <Form noValidate validated={validated} onSubmit={this.onSubmit}>
+                        <Form.Row>
+                            <Form.Group as={Col} controlId="validateEmail">
+                                <Form.Label>Email</Form.Label>
+                                <Form.Control
+                                    required
+                                    name="email"
+                                    type="email"
+                                    placeholder="Enter Email"
+                                    required
+                                />
+                                {/* <Form.Control.Feedback>Looks good!</Form.Control.Feedback> */}
+                            </Form.Group>
+                        </Form.Row>
+                        <Form.Row>
+                            <Form.Group as={Col} controlId="validatePassword">
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control
+                                    required
+                                    name="password"
+                                    type="password"
+                                    placeholder="Enter Password"
+                                />
+                                {/* <Form.Control.Feedback></Form.Control.Feedback> */}
+                            </Form.Group>
+                        </Form.Row>
+                        <Form.Row>
+                            <Button className="btn-bottom" type="submit" >Signin</Button>
+                        </Form.Row>
+                    </Form>
                 </div>
             </div>
         )
